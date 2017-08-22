dba2276dfb296a14bcd3a897106054fe7e62c268
simplefly <1059229782@qq.com>
2017-08-22 18:22:45 +0800

    服务器部分搭建好

---
+++ TicTacToe/src/com/tictactoe/constants/TicTacToeConstants.java
@@ -0,0 +1,10 @@
+package com.tictactoe.constants;
+
+public interface TicTacToeConstants {
+	public static int PLAYER1 = 1; // Indicate player1
+	public static int PLAYER2 = 2; // Indicate player2
+	public static int PLAYER1_WON = 1; // Indicate player1 won
+	public static int PLAYER2_WON = 2; // Indicate player2 won
+	public static int DRAW = 3; // Indicate a draw
+	public static int CONTINUE = 4; // Indicate to continue
+}
---
+++ TicTacToe/src/com/tictactoe/server/TicTacToeServer.java
@@ -0,0 +1,59 @@
+package com.tictactoe.server;
+
+import java.io.*;
+import java.net.*;
+import java.util.Date;
+
+import com.tictactoe.constants.TicTacToeConstants;
+
+public class TicTacToeServer implements TicTacToeConstants {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public TicTacToeServer(){
+		//Create a server socket
+		ServerSocket serverSocket;
+		try {
+			serverSocket = new ServerSocket(8000);
+			System.out.println(new Date() + ":Server started at socket 8000");
+			
+			//Number a session
+			int sessionNo = 1;
+			
+			//Ready to create a session for every two players
+			while(true){
+				System.out.println(new Date() + ": Wait for players to join session " + sessionNo);
+				
+				//Connect to player1
+				Socket player1 = serverSocket.accept();
+				System.out.println(new Date() + ": Player1 joined session " + sessionNo);
+				System.out.println("Player1 's IP address" + player1.getInetAddress().getHostAddress());
+				
+				//Notify that the player is Player1
+				new DataOutputStream(player1.getOutputStream()).writeInt(PLAYER1);
+				
+				//Connect to player2
+				Socket player2 = serverSocket.accept();
+				System.out.println(new Date() + ": Player2 joined session " + sessionNo);
+				System.out.println("Player2 's IP address" + player2.getInetAddress().getHostAddress());
+				
+				//Notify that the player is Player2
+				new DataOutputStream(player2.getOutputStream()).writeInt(PLAYER2);
+				
+				System.out.println(new Date() + ": Start a thread for session " + sessionNo);
+				
+				//Create a new thread for this session of two players
+				
+				
+				//Start the new thread
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+}
